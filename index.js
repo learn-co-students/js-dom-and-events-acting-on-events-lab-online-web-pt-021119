cb57f274445e1c8445380432eab8c65b9439b154
luizfper <luizfper@gmail.com>
2019-09-11 21:39:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+	document.querySelector('form').addEventListener('submit', function(event){
+		event.preventDefault()
+	})
 }
 
 preventRefreshOnSubmit()
 
+let input = document.querySelector('input')
+
 function retrieveEmployeeInformation(){
-	const tField = document.getElementsByTagName('input')[0]
-	return tField.value
+return input.value
 }
 
-function addNewElementAsLi(employeeName){
-	jQuery('ul.employee-list').append(`<li>${employeeName}</li>`)
+function addNewElementAsLi(){
+let employeeName = retrieveEmployeeInformation()
+document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+}
+
+
+function addNewLiOnClick(){
+let submit = document.querySelector('input[type="submit"]')
+submit.addEventListener('click', function(event){
+	addNewElementAsLi()
+	resetInput()
+})
+}
+
+function clearEmployeeListOnLinkClick(){
+let link = document.querySelector('a')
+let ul = document.querySelector('ul')
+link.addEventListener('click', function(event){
+	ul.innerHTML = ''
+})
+}
+
+function resetInput(){
+	document.querySelector('input').value = ''
 }
