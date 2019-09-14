3748ffc2f0372ddf5783746cc0c750aca785e685
cavu757 <jose.pehovazdiez@gmail.com>
2019-09-14 18:09:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
+      event.preventDefault();
+      retrieveEmployeeInformation();
+      addNewElementAsLi();
+      addNewLiOnClick();
+      clearEmployeeListOnLinkClick();
     })
+}
+
+function retrieveEmployeeInformation(){
+  return document.getElementsByName('name')[0].value.trim()
+}
+
+function addNewElementAsLi(){
+  let li = document.createElement('li');
+  let innertext = retrieveEmployeeInformation();
+  li.innerHTML = innertext;
+  if (innertext){ document.getElementsByClassName('employee-list')[0].appendChild(li)}
+}
+
+function addNewLiOnClick(){
+  document.querySelector('input').value='';
+  return;
+  document.querySelector('input[type="submit"]').addEventListener('click', function(e){
+    e.preventDefault();
+    addNewElementAsLi();
+  })
+}
+
+function clearEmployeeListOnLinkClick(){
+  return document.querySelector('a').addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelectorAll('ul')[0].innerHTML='';
+  })
 }
 
 preventRefreshOnSubmit()
