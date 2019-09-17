6d04eb471f45d92f1547b78f7225f1ed696a3bf3
lieby weintraub <liebahinda@gmail.com>
2019-09-17 05:29:08 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const ul= document.querySelector('ul')
+  const li= document.createElement('li')
+  li.innerHTML= retrieveEmployeeInformation()
+  ul.appendChild(li)
+  }
+  
+  function addNewLiOnClick() {
+  document.querySelector('form').addEventListener('submit',
+  function(submitEvent) {
+    addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  const clearLink = document.querySelector('a')
+  clearLink.addEventListener('click',
+    function(clickEvent) {
+      let ul = document.querySelector('ul')
+      ul.innerHTML = ''
+  })
+}
