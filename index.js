54c067d0d78f5e6750d9ee0f7d09af389b909866
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-09-24 16:59:33 +0000

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
-function retrieveEmployeeInformation(input) {
+function retrieveEmployeeInformation() {
+  let employee = document.querySelector('input').value;
   
+  return employee;
+}
+
+function addNewElementAsLi() {
+  let employee = retrieveEmployeeInformation();
+  let ul = document.querySelector('ul.employee-list');
+  
+  let li = document.createElement('li');
+  li.innerHTML = employee;
+  
+  ul.appendChild(li);
+}
+
+fuction addNewLiOnClick() {
+  return document.addEventListener('click', addNewElementAsLi, false);
 }
