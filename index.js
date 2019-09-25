fab318799efbffe48e2a0b1dc67c1f67768f6a20
github username <github email address>
2019-09-24 19:33:52 -0600

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ function preventRefreshOnSubmit(){
     })
 }
 
-preventRefreshOnSubmit()
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi(){
+  let node = document.createElement("li");                 
+  let textnode = document.createTextNode(retrieveEmployeeInformation()); 
+  node.appendChild(textnode); 
+  document.querySelector('input').appendChild(node);
+}
